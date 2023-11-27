83f6f64ba38f2e07434b3b6b333c2e0bb2e94256
Dean Mauro <selmerace@gmail.com>
2023-11-26 23:31:38 -0500

    Reorder params on pg proxy

--- src/core/pg.driver.ts
+++ src/core/pg.driver.ts
@@ -1,7 +1,7 @@
 import { Context } from "hono";
 import { DBCall, DBFactory } from "../types";
 
-async function PGFn<T>(endpoint: string, binding: Fetcher, query: object) {
+async function PGFn<T>(binding: Fetcher, endpoint: string, query: object) {
   const response = await binding.fetch(new Request(`http://worker/${endpoint}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -18,10 +18,10 @@ async function PGFn<T>(endpoint: string, binding: Fetcher, query: object) {
 export const PGFactory: DBFactory = (ctx: Context) => {
   return {
     fetch: async <T extends object>(query: DBCall) => {
-      return await PGFn<T>('query', ctx.env.DB_PROXY, query);
+      return await PGFn<T>(ctx.env.DB_PROXY, 'query', query);
     },
     raw: async <T extends object>(query: string) => {
-      return await PGFn<T>('text', ctx.env.DB_PROXY, { text: query });
+      return await PGFn<T>(ctx.env.DB_PROXY, 'text', { text: query });
     }
   };
 };
